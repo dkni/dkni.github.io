1e4f9153814ea42073dadd00538781e423ece3ee
dkni <3539729@gmail.com>
2016-11-26 18:48:44 +0300

    Added exceptional behavior handling for wrong input

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 function calculateFactorial() {
-	x = prompt("Factorial function: enter a number", 1);
+	var x = prompt("Factorial function: enter a number (0 - 170)", 1);
 	alert(x + "! = " + factorial(x));
 }
 
 var factorial = function(x){
-		if (x===1) return x;
+		if (x<0) return "Wrong operation";
+		if (x<=1) return x;
 		else return x * factorial(x-1);
 }
