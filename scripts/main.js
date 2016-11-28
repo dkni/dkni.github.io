94c3109c66c7aa11d0b433657f34391183543150
anatoliy <anatoliy>
2016-11-28 15:21:26 +0300

    Added BigInteger support, improved input validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 function calculateFactorial() {
-	var x = prompt("Factorial function: enter a number (0 - 170)", 1);
-	//alert(x + "! = " + factorial(x));
+	var x = prompt("Factorial function: enter a non-negative decimal number", 1);
 	var result = factorial(x);
-	if (x>21) //from here javascript switches into exponential notation
-		alert(x + "! = " + parseExponentialToReadable(String(result)) + '\n(exponential notation: ' + result + ')');
-	else
-		alert(x + "! = " + parseExponentialToReadable(String(result)));
+		alert(x + "! = " + parseExponentialToReadable(String(result)) + '\n\nThe result was additionally sent to the console.');
+		//if you want to copy the number
+		console.log(String(result));
+		//in case you're curious about the BigInteger's internal representation
+		console.log(result);
 }
 
 var factorial = function(x){
-		if (x < 0) return "Wrong operation";
+		if (x < 0 || isNaN(x)) return "Wrong operation";
 		if (x <= 1) return x;
-		else return x * factorial(x - 1);
+		//here using the https://github.com/jtobey/javascript-bignum library
+		else return BigInteger.multiply(x, factorial(x-1));
 }
 
 function parseExponentialToReadable(s){
@@ -19,12 +20,13 @@ function parseExponentialToReadable(s){
 	if (s.includes('e+')){
 		var number = s.split('e+');
 		number[0] = number[0].replace('.', '');
-		//adding as many zeroes as required by the power, taking the fraction into account
+		//adding as many zeroes as required by the power, taking the fraction into account, optimized concatenation for IE
 		var x = parseInt(number[1]) - (number[0].length - 1);
-		for (var i = 0; i < x; i++){
-			number[0] += '0';
+		for (var i = 1; i <= x; i++){
+			number[i] = '0';
 		}
-		return formatBigNumber(number[0]);
+		var result = number.join(''); 
+		return formatBigNumber(result);
 	}
 	//if not in exponential notation, return the original value, formatted
 	else {
