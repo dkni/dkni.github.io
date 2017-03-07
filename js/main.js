0296d380c922421591bef10692c6e81cb9b5bd10
dkni <3539729@gmail.com>
2017-03-07 14:38:00 +0300

    Fixed contents for White Fang

--- js/main.js
+++ js/main.js
@@ -1,53 +1,53 @@
-function calculateFactorial() {
-	var x = prompt("Factorial function: enter a non-negative integer number (decimal numeric system)", 1);
-	if (x === null){
-		return;
-	}
-	var integerPattern = new RegExp(/^\d+$/);
-	if (!integerPattern.test(x.toString())) {
-		alert ("Wrong input!");
-		console.log ("Wrong input: " + x);
-	} else {
-		var result = factorial(x);
-		alert(x + "! = " + formatBigNumber(String(result)) + '\n\nThe result was additionally sent to the console.');
-		//if you want to copy the number
-		console.log("Factorial of " + x + ":\n\n" + String(result));
+$(document).ready(function(event) {
+	function calculateFactorial() {
+		var x = prompt("Factorial function: enter a non-negative integer number (decimal numeric system)", 1);
+		if (x === null){
+			return;
 		}
-}
+		var integerPattern = new RegExp(/^\d+$/);
+		if (!integerPattern.test(x.toString())) {
+			alert ("Wrong input!");
+			console.log ("Wrong input: " + x);
+		} else {
+			var result = factorial(x);
+			alert(x + "! = " + formatBigNumber(String(result)) + '\n\nThe result was additionally sent to the console.');
+			//if you want to copy the number
+			console.log("Factorial of " + x + ":\n\n" + String(result));
+			}
+	}
 
-var factorial = function(x){
-		if (x < 0) return "Wrong operation";
-		if (x <= 1) return x;
-		//here using the https://github.com/jtobey/javascript-bignum library
-		else return BigInteger.multiply(x, factorial(x-1));
-}
+	var factorial = function(x){
+			if (x < 0) return "Wrong operation";
+			if (x <= 1) return x;
+			//here using the https://github.com/jtobey/javascript-bignum library
+			else return BigInteger.multiply(x, factorial(x-1));
+	}
 
-function parseExponentialToReadable(s){
-	//if in exponential notation
-	if (s.includes('e+')){
-		var number = s.split('e+');
-		number[0] = number[0].replace('.', '');
-		//adding as many zeroes as required by the power, taking the fraction into account, optimized concatenation for IE
-		var x = parseInt(number[1]) - (number[0].length - 1);
-		for (var i = 1; i <= x; i++){
-			number[i] = '0';
+	function parseExponentialToReadable(s){
+		//if in exponential notation
+		if (s.includes('e+')){
+			var number = s.split('e+');
+			number[0] = number[0].replace('.', '');
+			//adding as many zeroes as required by the power, taking the fraction into account, optimized concatenation for IE
+			var x = parseInt(number[1]) - (number[0].length - 1);
+			for (var i = 1; i <= x; i++){
+				number[i] = '0';
+			}
+			var result = number.join(''); 
+			return result;
 		}
-		var result = number.join(''); 
-		return result;
+		//if not in exponential notation, return the original value
+		else {
+			return s;
+		}
 	}
-	//if not in exponential notation, return the original value
-	else {
-		return s;
+
+	function formatBigNumber (s){
+		//separating every three digits with a comma, starting from the end
+		return s.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
 	}
-}
 
-function formatBigNumber (s){
-	//separating every three digits with a comma, starting from the end
-	return s.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-}
-
-$(document).ready(function(event) { 
-	$(".menubtn").jqxButton({ width: '150', height: '25', theme: 'energyblue' });
+	$(".menubtn").jqxButton({ width: '150', height: '25', theme: 'darkblue' });
 	
 	$.ajax({
 		url: "https://dkni.github.io/content/books/White Fang/contents.txt",
@@ -59,7 +59,19 @@ $(document).ready(function(event) {
 		$("#menuTree").jqxTree({
 			source: data,
 			width: '100%',
-			height: '100%'			
+			height: '100%',
+			theme: 'darkblue'
+		});
+		
+		$("#menuTree").on("select", function(event){
+			let item = $("#menuTree").jqxTree("getItem", event.args.element);
+			$.ajax({
+				url: "https://dkni.github.io/content/books/White Fang/Part I/" + item.label + ".txt",
+				type: "GET"
+			}).done(function(data){
+				$("#content").empty();
+				$("#content").html(data);
+			});
 		});
 	});
 		
@@ -72,12 +84,7 @@ $(document).ready(function(event) {
 	});
 		
 	$("#book").on("click", function(){
-		$.ajax({
-			url: "https://dkni.github.io/content/books/White Fang/Part I/Chapter 1.txt",
-			type: "GET"
-		}).done(function(data){
-			$("#content").html(data);
-		});
+		
 	});
 	
 	$("#fact").on("click", function(){
